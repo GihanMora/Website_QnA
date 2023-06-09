9e77b849e10b18ed0862c5e920b15056ea5280f0
Gihan Gamage <gihangamage.15@cse.mrt.ac.lk>
2023-06-10 01:23:50 +1000

    Update main.py

--- main.py
+++ main.py
@@ -17,9 +17,14 @@ st.write("---")
 st.header("Enter Website URL")
 site = st.text_input("Enter your URL here")
 
-crawl(site, 'simp.jl', follow_links=True)
-crawl_df = pd.read_json('simp.jl', lines=True)
-crawl_df = crawl_df[['body_text','header_links_text','og:title','h1', 'h2', 'h3', 'h4','h5','title']]
+
+if site is None:
+  st.info(f"""Enter Website to Build QnA Bot""")
+elif site:
+  st.write(str(len(site)) + " starting to crawl..")
+  crawl(site, 'simp.jl', follow_links=True)
+  crawl_df = pd.read_json('simp.jl', lines=True)
+  crawl_df = crawl_df[['body_text','header_links_text','og:title','h1', 'h2', 'h3', 'h4','h5','title']]
 
 st.write(crawl_df)
 
