373cc4eef36840e8fd9314a58b5a4fd001790ddf
Gihan Gamage <gihangamage.15@cse.mrt.ac.lk>
2023-06-10 11:51:11 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 # import langchain
 # from langchain.text_splitter import RecursiveCharacterTextSplitter
 # from langchain.embeddings.openai import OpenAIEmbeddings
-# from langchain.vectorstores import Chroma
+from langchain.vectorstores import Chroma
 # from langchain import OpenAI, VectorDBQA
 # from langchain.chains import RetrievalQAWithSourcesChain
 # import PyPDF2
