a1be092642b059252a492418075c90a9a4d6fd84
Gihan Gamage <gihangamage.15@cse.mrt.ac.lk>
2023-06-10 11:58:34 +1000

    Update main.py

--- main.py
+++ main.py
@@ -28,39 +28,46 @@ if site is None:
   st.info(f"""Enter Website to Build QnA Bot""")
 elif site:
 #   st.write(str(site) + " starting to crawl..")
-  with st.spinner(str(site) + " starting to crawl.."):
-    crawl(site, 'simp.jl', follow_links=False)
-    crawl_df = pd.read_json('simp.jl', lines=True)
-    st.write(len(crawl_df))
-    crawl_df = crawl_df[['body_text']]
-    st.write(crawl_df)
-    
-    #load df to langchain
-    loader = DataFrameLoader(crawl_df, page_content_column="body_text")
-    docs = loader.load()
-    
-    #chunking
-    char_text_splitter = CharacterTextSplitter(chunk_size=3000, chunk_overlap=10)
-    doc_texts = char_text_splitter.split_documents(docs)
-    
-    
-    #extract embeddings and build QnA Model
-    openAI_embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
-    vStore = Chroma.from_documents(doc_texts, openAI_embeddings)
-    
-    # Initialize VectorDBQA Chain from LangChain
-    model = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=vStore)
-    
-    if crawl_df:
-      st.write(str(len(uploaded_files)) + " document(s) loaded..")
-      st.header("Ask your data")
-      user_q = st.text_area("Enter your questions here")
-      if st.button("Get Response"):
-        try:
-          with st.spinner("Model is working on it..."):
-            result = model({"question":user_q}, return_only_outputs=True)
-            st.subheader('Your response:')
-            st.write(result['answer'])
-        except Exception as e:
-          st.error(f"An error occurred: {e}")
-          st.error('Oops, the GPT response resulted in an error :( Please try again with a different question.')
+  try:
+    with st.spinner(str(site) + " starting to crawl.."):
+      crawl(site, 'simp.jl', follow_links=False)
+      crawl_df = pd.read_json('simp.jl', lines=True)
+      st.write(len(crawl_df))
+      crawl_df = crawl_df[['body_text']]
+      st.write(crawl_df)
+
+      #load df to langchain
+      loader = DataFrameLoader(crawl_df, page_content_column="body_text")
+      docs = loader.load()
+
+      #chunking
+      char_text_splitter = CharacterTextSplitter(chunk_size=3000, chunk_overlap=10)
+      doc_texts = char_text_splitter.split_documents(docs)
+
+
+      #extract embeddings and build QnA Model
+      openAI_embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
+      vStore = Chroma.from_documents(doc_texts, openAI_embeddings)
+
+      # Initialize VectorDBQA Chain from LangChain
+      #deciding model
+      model_name = "gpt-3.5-turbo"
+      llm = OpenAI(model_name=model_name, openai_api_key = st.secrets["openai_api_key"]
+      model = VectorDBQA.from_chain_type(llm=llm, streaming=True), chain_type="stuff", vectorstore=vStore)
+
+      if crawl_df:
+        st.write(str(len(uploaded_files)) + " document(s) loaded..")
+        st.header("Ask your data")
+        user_q = st.text_area("Enter your questions here")
+        if st.button("Get Response"):
+          try:
+            with st.spinner("Model is working on it..."):
+              result = model({"question":user_q}, return_only_outputs=True)
+              st.subheader('Your response:')
+              st.write(result['answer'])
+          except Exception as e:
+            st.error(f"An error occurred: {e}")
+            st.error('Oops, the GPT response resulted in an error :( Please try again with a different question.')
+ except Exception as e:
+            st.error(f"An error occurred: {e}")
+            st.error('Oops, crawling resulted in an error :( Please try again with a different URL.')
