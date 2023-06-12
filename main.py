b4c40358b887e693e92d5461d04dfd2b01d437c4
Gihan Gamage <gihangamage.15@cse.mrt.ac.lk>
2023-06-12 15:02:38 +1000

    Update main.py

--- main.py
+++ main.py
@@ -31,55 +31,55 @@ st.set_page_config(layout="centered", page_title="Website QnA")
 st.header("Website QnA Bot 🤖")
 
 site = st.text_input("Enter your URL here")
-crawling = False
-if site is None:
-  st.info(f"""Enter Website to Build QnA Bot""")
-elif site and (not crawling):
-  st.write(str(site) + " starting to crawl..")
-  try:
+if st.button('Say hello'):
+  if site is None:
+    st.info(f"""Enter Website to Build QnA Bot""")
+  elif site and (not crawling):
+    st.write(str(site) + " starting to crawl..")
+    try:
 
-    my_bar = st.progress(0, text="Crawling in progress. Please wait.")
-    if os.path.exists("simp.jl"):
-      os.remove("simp.jl")
-    crawl(site, 'simp.jl', follow_links=False)
-    crawl_df = pd.read_json('simp.jl', lines=True)
-    st.write(len(crawl_df))
-    crawl_df = crawl_df[['body_text']]
-    my_bar.progress(50, text="Building Vector DB.")
-    st.write(crawl_df)
+      my_bar = st.progress(0, text="Crawling in progress. Please wait.")
+      if os.path.exists("simp.jl"):
+        os.remove("simp.jl")
+      crawl(site, 'simp.jl', follow_links=False)
+      crawl_df = pd.read_json('simp.jl', lines=True)
+      st.write(len(crawl_df))
+      crawl_df = crawl_df[['body_text']]
+      my_bar.progress(50, text="Building Vector DB.")
+      st.write(crawl_df)
 
-    #load df to langchain
-    loader = DataFrameLoader(crawl_df, page_content_column="body_text")
-    docs = loader.load()
+      #load df to langchain
+      loader = DataFrameLoader(crawl_df, page_content_column="body_text")
+      docs = loader.load()
 
-    #chunking
-    char_text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
-    doc_texts = char_text_splitter.split_documents(docs)
+      #chunking
+      char_text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
+      doc_texts = char_text_splitter.split_documents(docs)
 
 
-    #extract embeddings and build QnA Model
-    openAI_embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
-    vStore = Chroma.from_documents(doc_texts, openAI_embeddings)
+      #extract embeddings and build QnA Model
+      openAI_embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
+      vStore = Chroma.from_documents(doc_texts, openAI_embeddings)
 
-    # Initialize VectorDBQA Chain from LangChain
-    #deciding model
-    model_name = "gpt-3.5-turbo"
-    llm = OpenAI(model_name=model_name, openai_api_key = st.secrets["openai_api_key"])
-    model = VectorDBQA.from_chain_type(llm=llm, chain_type="stuff", vectorstore=vStore)
-    my_bar.progress(100, text="Model is ready.")
-    crawling = True
-    if len(crawl_df):
-      st.header("Ask your data")
-      user_q = st.text_input("Enter your questions here")
-      if st.button("Get Response"):
-        try:
-          with st.spinner("Model is working on it..."):
-            result = model({"query":user_q}, return_only_outputs=True)
-            st.subheader('Your response:')
-            st.write(result['answer'])
-        except Exception as e:
-          st.error(f"An error occurred: {e}")
-          st.error('Oops, the GPT response resulted in an error :( Please try again with a different question.')
-  except Exception as e:
+      # Initialize VectorDBQA Chain from LangChain
+      #deciding model
+      model_name = "gpt-3.5-turbo"
+      llm = OpenAI(model_name=model_name, openai_api_key = st.secrets["openai_api_key"])
+      model = VectorDBQA.from_chain_type(llm=llm, chain_type="stuff", vectorstore=vStore)
+      my_bar.progress(100, text="Model is ready.")
+      crawling = True
+      if len(crawl_df):
+        st.header("Ask your data")
+        user_q = st.text_input("Enter your questions here")
+        if st.button("Get Response"):
+          try:
+            with st.spinner("Model is working on it..."):
+              result = model({"query":user_q}, return_only_outputs=True)
+              st.subheader('Your response:')
+              st.write(result['answer'])
+          except Exception as e:
             st.error(f"An error occurred: {e}")
-            st.error('Oops, crawling resulted in an error :( Please try again with a different URL.')
+            st.error('Oops, the GPT response resulted in an error :( Please try again with a different question.')
+    except Exception as e:
+              st.error(f"An error occurred: {e}")
+              st.error('Oops, crawling resulted in an error :( Please try again with a different URL.')
