71ec9380245a1a4b7052b27ad86f035447522bb5
Gihan Gamage <gihangamage.15@cse.mrt.ac.lk>
2023-06-12 14:27:16 +1000

    Update main.py

--- main.py
+++ main.py
@@ -38,34 +38,36 @@ if site is None:
 elif site:
 #   st.write(str(site) + " starting to crawl..")
   try:
-    with st.spinner(str(site) + " starting to crawl.."):
-      if os.path.exists("simp.jl"):
-        os.remove("simp.jl")
-      crawl(site, 'simp.jl', follow_links=False)
-      crawl_df = pd.read_json('simp.jl', lines=True)
-      st.write(len(crawl_df))
-      crawl_df = crawl_df[['body_text']]
-      st.write(crawl_df)
+    
+    my_bar = st.progress(0, text="Crawling in progress. Please wait.")
+    if os.path.exists("simp.jl"):
+      os.remove("simp.jl")
+    crawl(site, 'simp.jl', follow_links=False)
+    crawl_df = pd.read_json('simp.jl', lines=True)
+    st.write(len(crawl_df))
+    crawl_df = crawl_df[['body_text']]
+    my_bar.progress(50, text="Building Vector DB.")
+    st.write(crawl_df)
 
-      #load df to langchain
-      loader = DataFrameLoader(crawl_df, page_content_column="body_text")
-      docs = loader.load()
+    #load df to langchain
+    loader = DataFrameLoader(crawl_df, page_content_column="body_text")
+    docs = loader.load()
 
-      #chunking
-      char_text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
-      doc_texts = char_text_splitter.split_documents(docs)
+    #chunking
+    char_text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
+    doc_texts = char_text_splitter.split_documents(docs)
 
 
-      #extract embeddings and build QnA Model
-      openAI_embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
-      vStore = Chroma.from_documents(doc_texts, openAI_embeddings)
+    #extract embeddings and build QnA Model
+    openAI_embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
+    vStore = Chroma.from_documents(doc_texts, openAI_embeddings)
 
-      # Initialize VectorDBQA Chain from LangChain
-      #deciding model
-      model_name = "gpt-3.5-turbo"
-      llm = OpenAI(model_name=model_name, openai_api_key = st.secrets["openai_api_key"])
-      model = VectorDBQA.from_chain_type(llm=llm, chain_type="stuff", vectorstore=vStore)
-
+    # Initialize VectorDBQA Chain from LangChain
+    #deciding model
+    model_name = "gpt-3.5-turbo"
+    llm = OpenAI(model_name=model_name, openai_api_key = st.secrets["openai_api_key"])
+    model = VectorDBQA.from_chain_type(llm=llm, chain_type="stuff", vectorstore=vStore)
+    my_bar.progress(100, text="Model is ready.")
     if len(crawl_df):
       st.header("Ask your data")
       user_q = st.text_area("Enter your questions here")
