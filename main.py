b243017fc0f923eb68f21553bc26bf05040310fe
Gihan Gamage <gihangamage.15@cse.mrt.ac.lk>
2023-06-12 11:49:23 +1000

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ with col1:
     st.image(image, width=80)
 with col2:
     st.header('    Geek Avenue')
-st.write("---")
+st.write(":heavy_minus_sign:" * 34) # horizontal separator line.
 
 
 st.header("Website QnA Bot 🤖")
