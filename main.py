f403d6309339166a782befebd6770fdc67b7a3b7
Gihan Gamage <gihangamage.15@cse.mrt.ac.lk>
2023-06-12 15:39:53 +1000

    Update main.py

--- main.py
+++ main.py
@@ -31,50 +31,50 @@ st.set_page_config(layout="centered", page_title="Website QnA")
 st.header("Website QnA Bot 🤖")
 state = st.session_state
 site = st.text_input("Enter your URL here")
+if st.button("Build Model"):
+  if site is None:
+    st.info(f"""Enter Website to Build QnA Bot""")
+  elif site  and ("crawling" not in state):
+    st.write(str(site) + " starting to crawl..")
+    try:
 
-if site is None:
-  st.info(f"""Enter Website to Build QnA Bot""")
-elif site  and ("crawling" not in state):
-  st.write(str(site) + " starting to crawl..")
-  try:
+      my_bar = st.progress(0, text="Crawling in progress. Please wait.")
+      if os.path.exists("simp.jl"):
+        os.remove("simp.jl")
+      crawl(site, 'simp.jl', follow_links=False)
+      crawl_df = pd.read_json('simp.jl', lines=True)
+      st.write(len(crawl_df))
+      crawl_df = crawl_df[['body_text']]
+      my_bar.progress(50, text="Building Vector DB.")
+      st.write(crawl_df)
 
-    my_bar = st.progress(0, text="Crawling in progress. Please wait.")
-    if os.path.exists("simp.jl"):
-      os.remove("simp.jl")
-    crawl(site, 'simp.jl', follow_links=False)
-    crawl_df = pd.read_json('simp.jl', lines=True)
-    st.write(len(crawl_df))
-    crawl_df = crawl_df[['body_text']]
-    my_bar.progress(50, text="Building Vector DB.")
-    st.write(crawl_df)
+      #load df to langchain
+      loader = DataFrameLoader(crawl_df, page_content_column="body_text")
+      docs = loader.load()
 
-    #load df to langchain
-    loader = DataFrameLoader(crawl_df, page_content_column="body_text")
-    docs = loader.load()
-
-    #chunking
-    text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=0, separators=[" ", ",", "\n"]
-    )
-    doc_texts = text_splitter.split_documents(docs)
+      #chunking
+      text_splitter = RecursiveCharacterTextSplitter(
+      chunk_size=1000, chunk_overlap=0, separators=[" ", ",", "\n"]
+      )
+      doc_texts = text_splitter.split_documents(docs)
 
 
-    #extract embeddings and build QnA Model
-    openAI_embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
-    vStore = Chroma.from_documents(doc_texts, openAI_embeddings)
+      #extract embeddings and build QnA Model
+      openAI_embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
+      vStore = Chroma.from_documents(doc_texts, openAI_embeddings)
 
-    # Initialize VectorDBQA Chain from LangChain
-    #deciding model
-    model_name = "gpt-3.5-turbo"
-    llm = OpenAI(model_name=model_name, openai_api_key = st.secrets["openai_api_key"])
-    model = VectorDBQA.from_chain_type(llm=llm, chain_type="stuff", vectorstore=vStore)
-    my_bar.progress(100, text="Model is ready.")
-    st.session_state['crawling'] = True
-    st.session_state['model'] = model 
-    
-  except Exception as e:
-            st.error(f"An error occurred: {e}")
-            st.error('Oops, crawling resulted in an error :( Please try again with a different URL.')
+      # Initialize VectorDBQA Chain from LangChain
+      #deciding model
+      model_name = "gpt-3.5-turbo"
+      llm = OpenAI(model_name=model_name, openai_api_key = st.secrets["openai_api_key"])
+      model = VectorDBQA.from_chain_type(llm=llm, chain_type="stuff", vectorstore=vStore)
+      my_bar.progress(100, text="Model is ready.")
+      st.session_state['crawling'] = True
+      st.session_state['model'] = model 
+
+    except Exception as e:
+              st.error(f"An error occurred: {e}")
+              st.error('Oops, crawling resulted in an error :( Please try again with a different URL.')
      
 if site and ("crawling" in state):
       st.header("Ask your data")
